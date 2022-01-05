a5eef4687264af6a92f8028d1e19e2584558e763
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2022-01-05 00:55:52 -0300

    First commit

---
+++ src/test/java/com/vinolemos/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinolemos.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
