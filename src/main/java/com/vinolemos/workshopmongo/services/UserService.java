4c93b0d740ab81f34c098972e16c040a7facf770
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2022-01-06 17:33:00 -0300

    Return user by ID

--- src/main/java/com/vinolemos/workshopmongo/services/UserService.java
+++ src/main/java/com/vinolemos/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.vinolemos.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vinolemos.workshopmongo.domain.User;
 import com.vinolemos.workshopmongo.repository.UserRepository;
+import com.vinolemos.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
