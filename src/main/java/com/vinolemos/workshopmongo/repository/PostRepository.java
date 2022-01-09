52406bd9c0a52cd98d33fd7158841e87e9be5856
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2022-01-09 19:23:36 -0300

    @Query simple search

--- src/main/java/com/vinolemos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/vinolemos/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.vinolemos.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.vinolemos.workshopmongo.domain.Post;
@@ -10,6 +11,8 @@ import com.vinolemos.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{'title': {$regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
